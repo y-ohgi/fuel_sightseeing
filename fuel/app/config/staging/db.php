3c3f510e1de131fc9cf772ee225a47667d760d0c
y-ohgi <ohgi.211@gmail.com>
2016-05-18 05:50:28 +0900

    heroku用db接続設定

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -2,13 +2,19 @@
 /**
  * The staging database settings. These get merged with the global settings.
  */
+$dbConfigPattern = '/mysql:\/\/(?:([^:^@]+)(?::([^@]+))?@)?([^:^\/]+)(?::(\d+))?\/([^?]+)/';
+if (preg_match($dbConfigPattern, $_SERVER["CLEARDB_DATABASE_URL"], $matches)) {
+    list($dbConfig, $dbuser, $dbpass, $dbhost, $dbport, $dbname) = $matches;
 
-return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
-);
+    
+
+    return array(
+        'default' => array(
+            'connection'  => array(
+                'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
+                'username'   => 'fuel_app',
+                'password'   => 'super_secret_password',
+            ),
+        ),
+    );
+}
