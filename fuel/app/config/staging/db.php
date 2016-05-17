e57dfd95d8a127f42c27b61461363ce79544cadd
y-ohgi <ohgi.211@gmail.com>
2016-05-18 05:56:29 +0900

    fix heroku db config

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -2,18 +2,17 @@
 /**
  * The staging database settings. These get merged with the global settings.
  */
+
 $dbConfigPattern = '/mysql:\/\/(?:([^:^@]+)(?::([^@]+))?@)?([^:^\/]+)(?::(\d+))?\/([^?]+)/';
 if (preg_match($dbConfigPattern, $_SERVER["CLEARDB_DATABASE_URL"], $matches)) {
     list($dbConfig, $dbuser, $dbpass, $dbhost, $dbport, $dbname) = $matches;
 
-    
-
     return array(
         'default' => array(
             'connection'  => array(
-                'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-                'username'   => 'fuel_app',
-                'password'   => 'super_secret_password',
+                'dsn'        => 'mysql:host='.$dbhost.';dbname='.$dbname,
+                'username'   => $dbuser,
+                'password'   => $dbpass,
             ),
         ),
     );
